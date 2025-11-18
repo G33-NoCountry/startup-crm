74f7666de5ca01023316fb00902491760670638e
Angeles Orquera <angl3273000@gmail.com>
2025-11-18 14:32:46 -0300

    feat: tailwind config & routing

---
+++ frontend/src/app/(dashboard)/kanban/page.tsx
@@ -0,0 +1,7 @@
+export default function KanbanPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-2xl font-bold">Página de kanban </h1>
+        </div>
+    );
+}
