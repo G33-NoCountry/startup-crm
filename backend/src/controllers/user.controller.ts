185830465db57b5da6978abfb22969488e115ca9
BarreraJoel <joelmanuelbar@gmail.com>
2025-11-22 22:48:05 -0300

    docs(user): correccion de documentacion

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -81,6 +81,12 @@ export class UserController {
    *     tags: [Users]
    *     security:
    *       - bearerAuth: []
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/UpdateUserRequest'
    *     responses:
    *        200:
    *         description: Se actualiza el usuario
--- backend/src/docs/schemas/index.ts
+++ backend/src/docs/schemas/index.ts
@@ -1,13 +1,15 @@
-import authSchemas  from "./request/auth/auth.schema";
 import codeSchemas  from "./response/code-response.schema";
 import userSchemas  from "./models/user/user.schema";
 import paginateSchemas  from "./response/paginate.schema";
-import adminSchemas  from "./request/admin/admin.schema";
+import authRequestSchemas  from "./request/auth.schema";
+import adminRequestSchemas  from "./request/admin.schema";
+import userRequestSchemas  from "./request/user.schema";
 
 export default {
-    ...authSchemas,
+    ...authRequestSchemas,
     ...codeSchemas,
     ...userSchemas,
     ...paginateSchemas,
-    ...adminSchemas
+    ...adminRequestSchemas,
+    ...userRequestSchemas
 };
