f9f1e42a94f19b8097d0cf0fbc0ba467bb2b9db3
BarreraJoel <joelmanuelbar@gmail.com>
2025-11-28 20:23:47 -0300

    fix: permisos de rutas contacts

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { checkJwtMiddleware } from "../middlewares/authenticate.middleware";
 import { AdminController } from "../controllers/admin.controller";
-import { isAdmin } from "../middlewares/is-admin.middleware";
 import { queryParamPaginateValidator } from "../validators/param/query-param.validator";
 import { registerUserAdminValidator } from "../validators/admin/register-user-admin.validator";
 import { validateParam } from "../validators/param/param.validator";
@@ -9,11 +8,12 @@ import { updateUserAdminValidator } from "../validators/admin/update-user-admin.
 import { userExists } from "../middlewares/user-exist.middleware";
 import { sanitizeBody } from "../middlewares/sanitize.middlewares";
 import validateRequestMiddleware from "../middlewares/validate-request.middleware";
+import { acceptRoleMiddleware } from "../middlewares/check-role.middleware";
 
 const router = Router();
 const adminController = new AdminController;
 
-router.use(checkJwtMiddleware, isAdmin);
+router.use(checkJwtMiddleware, acceptRoleMiddleware("Admin"));
 router.get('/users', queryParamPaginateValidator, validateRequestMiddleware, adminController.getUserList);
 router.post('/users', registerUserAdminValidator, validateRequestMiddleware, sanitizeBody, adminController.createUser);
 router.patch(
